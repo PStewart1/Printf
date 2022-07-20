4143d519414727441d7ff71abc6cb45a9324e8fa
PStewart1 <61563756+PStewart1@users.noreply.github.com>
2022-07-20 14:11:44 -0500

    Create prompt.c

---
+++ prompt.c
@@ -0,0 +1,18 @@
+	#include <stdio.h>
+        #include <stdlib.h>
+        #include <unistd.h>
+
+       int main()
+       {
+           char *line = NULL;
+           size_t len = 0;
+           ssize_t nread;
+
+           printf("$ ");
+           nread = getline(&line, &len, stdin);
+           write(1, line, nread);
+          
+
+           free(line);
+           exit(EXIT_SUCCESS);
+       }

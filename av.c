aaf139d0436a2ec4f2cf947167547e782a5c245f
PStewart1 <61563756+PStewart1@users.noreply.github.com>
2022-07-20 14:13:29 -0500

    Create av.c

---
+++ av.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - PID
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char *av[])
+{
+	int i;
+
+	for (i = 0; *av[i] != '\0'; i++)
+	{
+		printf("%s\n", av[i]);
+	}
+	printf("[%d]\n", ac);
+	return (0);
+}
